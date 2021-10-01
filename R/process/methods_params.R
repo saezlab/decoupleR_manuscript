0ba30dfabb1e92b8c018485b788fa27c27954ba7
PauBadiaM <paubadiamompel@gmail.com>
2021-10-01 13:48:19 +0200

    Updated method params

--- R/process/methods_params.R
+++ R/process/methods_params.R
@@ -1,22 +1,21 @@
 # List of the methods to call
 stats_list = list(c('udt','mdt','aucell','wmean','wsum','ulm','mlm','viper','gsva','ora','fgsea'))
 
+# Random seed
+seed = 42
+nproc = 4
+
 # List of options for each method
 opts_list <- list(list(
-  udt = list(min_n = 20),
-  mdt = list(trees = 10, min_n = 20, nproc = 4),
-  aucell = list(nproc=4),
-  wmean = list(times=1000),
-  wsum = list(times=1000),
-  ulm = list(center=FALSE),
-  mlm = list(center=FALSE),
-  viper = list(verbose = FALSE,
-               minsize = 0,
-               .mor = "mor",
-               .likelihood = "likelihood",
-               pleiotropy = T,
-               eset.filter = F),
+  aucell = list(nproc=nproc, seed=seed),
+  fgsea = list(times=100, nproc=nproc, seed=seed),
   gsva = list(verbose = FALSE, method = "gsva"),
-  ora = list(n_up=300, n_bottom=300, n_background=20000),
-  fgsea = list(force_ties = T, times=1000, nproc=4)
+  mdt = list(sparse=FALSE, center=FALSE, na.rm=FALSE, trees = 10, min_n = 20, nproc = nproc, seed=seed),
+  mlm = list(sparse=FALSE, center=FALSE, na.rm=FALSE),
+  ora = list(n_up=300, n_bottom=300, n_background=20000, with_ties = TRUE),
+  udt = list(sparse=FALSE, center=FALSE, na.rm=FALSE, min_n = 20, seed=seed),
+  ulm = list(sparse=FALSE, center=FALSE, na.rm=FALSE),
+  viper = list(verbose = FALSE, minsize = 0, pleiotropy = T, eset.filter = F),
+  wmean = list(times=100, sparse=TRUE, randomize_type = "rows", seed=seed),
+  wsum = list(times=100, sparse=TRUE, randomize_type = "rows", seed=seed)
 ))
